a30c7fbfdcfd2e998415c3c4cc57ac8f9056f769
InfernalHeir123 <devashishkjoshi1@gmail.com>
2023-12-16 23:45:54 +0000

    added favicon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import './App.css';
-import Navbar from "./components/navbar/Navbar.js";
+import React, { useEffect } from 'react';
+import Navbar from './components/navbar/Navbar';
+import { Helmet } from 'react-helmet';
+import PhindrLogo from './phindr.png';
 
 function App() {
+  useEffect(() => {
+    // Change website title
+    document.title = 'Phindr | Medical Care for Everyone';
+
+    // Change favicon dynamically
+    const link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+
+    // Use the actual path or URL of the image for the favicon
+    link.href = PhindrLogo;
+
+    document.head.appendChild(link);
+
+    return () => {
+      // If needed, perform cleanup when the component is unmounted
+      // For example, revert changes made in the 'useEffect'
+    };
+  }, []); // Empty dependency array to ensure the effect runs only once on mount
+
   return (
     <div className="App">
+      <Helmet>
+        {/* Add additional meta tags or other header elements here */}
+      </Helmet>
       <Navbar />
     </div>
   );
