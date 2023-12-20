1ed5bbc0e8ea961277986a8cbb91bd7a117942fc
InfernalHeir123 <devashishkjoshi1@gmail.com>
2023-12-20 14:15:16 +0000

    Added Storepage Content

--- src/components/store_page/prodCat.js
+++ src/components/store_page/prodCat.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import './prodCat.css';
-import item1 from './item1.png'
+import products from './products';
 
 function ProdCat() {
-    const handleClick = () => {
-        console.log('Button clicked!');
-      };
-    
-    return (
-      <div className='prod-cat'>
-        <div className='item-container'>
-          <img src={item1} alt='Dettol Hand Sanitizer' className='item1' />
+  const handleClick = () => {
+    console.log('Button clicked!');
+  };
+
+  return (
+    <div className='prod-cat'>
+      {products.map((product, index) => (
+        <div key={product.id} className='product-container'>
+          <div className='item-container'>
+            {/* Use require with a relative path to dynamically load the image */}
+            <img src={require(`./${product.image}`)} alt={product.name} className='item-image' />
+          </div>
+          <div className='item-info'>
+            <h3>{product.name}</h3>
+            <p>{product.description}</p>
+            <h4>{product.price}</h4>
+            <button onClick={handleClick}>Add to Basket</button>
+          </div>
         </div>
-        <div className='item-info'>
-          <h3>Dettol Hand Sanitizer</h3>
-          <h3>500ml</h3>
-          <h4>£7.00</h4>
-          <button onClick={handleClick}>Add to Basket</button>
-        </div>
-      </div>
-    );
-  }
+      ))}
+    </div>
+  );
+}
 
 export default ProdCat;
---
+++ src/components/store_page/products.js
@@ -0,0 +1,60 @@
+const products = [
+  {
+    id: 1,
+    name: 'Dettol Hand Sanitizer',
+    description: '500ml',
+    price: '£7.00',
+    image: 'item1.png',
+  },
+  {
+    id: 2,
+    name: 'Elastoplast Liquid Bandage',
+    description: '200ml',
+    price: '£9.99',
+    image: 'item1.png',
+  },
+  {
+    id: 3,
+    name: 'TCP Antiseptic Solution',
+    description: '250ml',
+    price: '£5.50',
+    image: 'item1.png',
+  },
+  {
+    id: 4,
+    name: 'Dettol Antibacterial Soap',
+    description: 'Pack of 3',
+    price: '£4.50',
+    image: 'item1.png',
+  },
+  {
+    id: 5,
+    name: 'Elastoplast Wound Wash Spray',
+    description: '150ml',
+    price: '£6.75',
+    image: 'item1.png',
+  },
+  {
+    id: 6,
+    name: 'TCP Hand Sanitizer Gel',
+    description: '500ml',
+    price: '£8.99',
+    image: 'item1.png',
+  },
+  {
+    id: 7,
+    name: 'Dettol Disinfectant Spray',
+    description: '300ml',
+    price: '£6.50',
+    image: 'item1.png',
+  },
+  {
+    id: 8,
+    name: 'Elastoplast Burn Relief Gel',
+    description: '100ml',
+    price: '£7.25',
+    image: 'item1.png',
+  },
+];
+
+export default products;
