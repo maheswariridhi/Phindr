ea5ecfe875b904a7decf01066786843c8b763243
InfernalHeir123 <devashishkjoshi1@gmail.com>
2023-12-28 22:26:05 +0000

    added Basket

--- src/components/store_page/basket.js
+++ src/components/store_page/basket.js
@@ -1,25 +1,46 @@
-import { React, useState } from 'react';
+import React, { useState } from 'react';
 import './basket.css';
 import samplePic from './item1.png';
 import productsArray from './products';
 import { useAppState } from './AppStateContext';
 
+import Invoice from './invoice';
 
 function Basket() {
-  const [quantities, setQuantities] = useState({});
+  const [quantities, setQuantities] = useState(() => {
+    const initialQuantities = {};
+
+    // Initialize quantities for each product with 0
+    productsArray.forEach((product) => {
+      initialQuantities[product.ID] = 0;
+    });
+
+    return initialQuantities;
+  });
+
   const { itemAddedToBasket } = useAppState();
 
   const indexesArray = Object.keys(itemAddedToBasket).map(Number);
-  const products = productsArray.filter(product => indexesArray.includes(product.ID));
+  const products = productsArray.filter((product) => indexesArray.includes(product.ID));
+
+  const handleQuantityChange = (productId, value, limit) => {
+    setQuantities((prevQuantities) => {
+      const newQuantities = {
+        ...prevQuantities,
+        [productId]: Math.max(0, value), // Ensure the quantity is at least 0
+      };
+  
+      // Check if product.LIMIT is equal to 1 and value is greater than 1
+      if (limit === 1 && newQuantities[productId] > 1) {
+        // Limit the quantity to 1
+        newQuantities[productId] = 1;
+      }
+  
+      return newQuantities;
+    });
+  };
 
   console.log(products);
-
-  const handleQuantityChange = (productId, value) => {
-    setQuantities((prevQuantities) => ({
-      ...prevQuantities,
-      [productId]: value,
-    }));
-  };
 
   return (
     <div className='Basket-Container'>
@@ -27,32 +48,32 @@ function Basket() {
       <div className='product1-container'>
         {products.map((product) => (
           <div className='quantity-slide' key={product.ID}>
-
             <div className='prod-info'>
               <div className='row-1'>
                 <div className='sub-row'>
-                  <h2>{product.BRAND} - {product.TYPE}</h2>
+                  <h2>
+                    {product.BRAND} - {product.TYPE}
+                  </h2>
                   <h4> &nbsp; &nbsp; ({product.QTY}) </h4>
                 </div>
-
-                  <h5>Quantity: </h5>
-                  <input
-                    type='number'  // Use type 'number'
-                    placeholder='Enter quantity'
-                    value={quantities[product.ID] || ''}
-                    onChange={(e) => handleQuantityChange(product.ID, e.target.value)}
-                    className='qty-input'
-                  />
-
+                <h5>Quantity: </h5>
+                <input
+                  type='number' // Use type 'number'
+                  placeholder='Enter quantity'
+                  value={quantities[product.ID]}
+                  onChange={(e) =>
+                    handleQuantityChange(product.ID, e.target.value, product.LIMIT)
+                  }
+                  className='qty-input'
+                />
               </div>
               <h3>£{product.SPRICE.toFixed(2)}</h3>
               <h4>Serial Number: 100537{product.ID}</h4>
             </div>
           </div>
         ))}
-        <div className='bill-slide'>
-          
-        </div>
+
+        <Invoice products={products} quantities={quantities} />
       </div>
     </div>
   );
---
+++ src/components/store_page/invoice.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import './basket.css';
+import './invoice.css';
+
+function Invoice({ products, quantities }) {
+  function generateRandomSerialNumber() {
+    const randomNumber = Math.floor(Math.random() * 900000) + 100000;
+    return '100537' + randomNumber;
+  }
+
+  const randomSerialNumber = generateRandomSerialNumber();
+
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, '0');
+  const day = String(today.getDate()).padStart(2, '0');
+  const formattedDate = `${day}/${month}/${year}`;
+
+  // Initialize total cost
+  let totalCost = 0;
+
+  return (
+    <div className='invoice-container'>
+      <div>
+        <h1>Phindr Ltd.</h1>
+        <h2>{formattedDate}</h2>
+      </div>
+
+      <p>#{randomSerialNumber}</p>
+
+      <div className='product-invoice'>
+        {products.map((product) => {
+          // Calculate the total price for the current product
+          const totalPrice = Number(product.SPRICE) * quantities[product.ID];
+
+          // Add the current product's total price to the overall total cost
+          totalCost += totalPrice;
+
+          return (
+            <div key={product.ID} className='test-class'>
+              <div className='invoice-r1'>
+                <p>
+                  {product.BRAND} - {product.TYPE}
+                </p>
+                <p>Total Price: £{totalPrice.toFixed(2)}</p>
+              </div>
+              <p>Quantity: {quantities[product.ID]}</p>
+            </div>
+          );
+        })}
+      </div>
+
+      {/* Display the total cost after mapping through all products */}
+      <div className='total-cost'>
+        <p>Total Cost: £{totalCost.toFixed(2)}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Invoice;
