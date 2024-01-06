5a4868b4f10a80005eb234ba0a962b66478fdb82
edagirgin <eda.girgin21@imperial.ac.uk>
2024-01-06 19:17:01 +0000

    Admin home dashboard css and formatting completed (ready for graphs)

--- src/components/AdminApp/home_page/AdminHomepage.js
+++ src/components/AdminApp/home_page/AdminHomepage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './AdminHomepage.css';
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import locationImage from '../admin_dashboard/location.png';
 import AdminNavbar from "../navbar/AdminNavbar";
 
@@ -7,23 +8,38 @@ const AdminHomepage = () => {
     return (
         <div >
             
-            <img src={locationImage} alt="Location" className="location-icon"/>
+            <h3 className="dashboard-title">Dashboard</h3>
+            <img src={locationImage} alt="Location" className="location-icon-admin"/>
             <div className="lowest_stock">
-                <p>lowest stock</p>
+                <p className="dashboard-title-box">Lowest stock</p>
             </div>
 
             <div className="grid-row">
                 <div className="daily_stats">
-                    <p>daily stats</p>
+                    <p className="dashboard-title-box">Daily stats</p>
                 </div>
 
                 <div className="stacked-boxes">
                     <div className="smallest_boxes">
-                        <p>total balance</p>
+                        <div className="transaction-history-container">
+                            <p className="dashboard-title-box">Total Balance</p>
+                            <p className="dashboard-title-box">£XXX,XXX</p>
+                            <div className="in-out-container">
+                                <p className="text1>">Income</p>
+                                <p className="text2">Expenses</p>
+                            </div>
+                        </div>
                     </div>
 
                     <div className="smallest_boxes">
-                        <p>Transaction History</p>
+                        <div className="transaction-history-container">
+                            <p className="dashboard-title-box">Transaction History</p>
+                            <Link to = "../admin-transactions">
+                                <p className="view-all"> View all </p>
+                            </Link>    
+                        </div>
+                        
+                        
                     </div>
                 </div>
                 
