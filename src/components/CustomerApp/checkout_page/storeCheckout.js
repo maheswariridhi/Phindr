ea30d1dad24ccf0a735236bdfe6ea47335a67582
Holmium165 <hoyan.siu@gmail.com>
2024-01-15 06:56:07 +0000

    Update storeCheckout.js

--- src/components/CustomerApp/checkout_page/storeCheckout.js
+++ src/components/CustomerApp/checkout_page/storeCheckout.js
@@ -21,63 +21,138 @@ const StoreCheckoutPage = () => {
         history.push('/map'); // Replace '/another-page' with the path of the page you want to navigate to
       };
 
+      // State for customer details
+    const [email, setEmail] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [address, setAddress] = useState('');
+    const [city, setCity] = useState('');
+    const [postcode, setPostcode] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [cardName, setCardName] = useState('');
+    const [cardNumber, setCardNumber] = useState('');
+    const [expiryMonth, setExpiryMonth] = useState('');
+    const [expiryYear, setExpiryYear] = useState('');
+    const [securityCode, setSecurityCode] = useState('');
 
+    const [isLoading, setIsLoading] = useState(false);
+    const [showErrorMessage, setShowErrorMessage] = useState(false); // New state for error message visibility
+
+ 
+
+    // JSX for the checkout page  
     return (
         <div className='grid-container1'>
             <div className='form-container1'>
-
-                <h3 className="text-checkout" style={{paddingTop: 40}}>Contact</h3>
-                <p className="text-checkout">Email</p>
                 <form className='checkout-form'>
-                    <input type="text" id="email" name="email" placeholder='...' className='input-field-shorter'/>
-                </form>
-           
+                    {/* Contact Information Section */}
+                    <h3 className="text-checkout" style={{paddingTop: 40}}>Contact</h3>
+                    <input
+                        type="text"
+                        id="email"
+                        name="email"
+                        placeholder='Email'
+                        className='input-field-shorter'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
 
-                <h3 className="text-checkout" style={{paddingTop :20}}>Shipping address </h3>
-                <div className='grid-equal'>
-                    <div>
-                        <p className="text-checkout">First Name</p>
-                        <form className='checkout-form'>
-                            <input type="text" id="first-name" name="first-email" placeholder='...' className='input-field-checkout' />
-                        </form>
+                    {/* Shipping Address Section */}
+                    <h3 className="text-checkout" style={{paddingTop :20}}>Shipping address </h3>
+                    <div className='grid-equal'>
+                        <div>
+                            <input
+                                type="text"
+                                id="first-name"
+                                name="firstName"
+                                placeholder='First Name'
+                                className='input-field-checkout'
+                                value={firstName}
+                                onChange={(e) => setFirstName(e.target.value)}
+                            />
+                        </div>
+                        <div>
+                            <input
+                                type="text"
+                                id="last-name"
+                                name="lastName"
+                                placeholder='Last Name'
+                                className='input-field-checkout'
+                                value={lastName}
+                                onChange={(e) => setLastName(e.target.value)}
+                            />
+                        </div>
                     </div>
-                    <div>
-                        <p className="text-checkout">Last Name</p>
-                        <form className='checkout-form'>
-                            <input type="text" id="last-name" name="last-name" placeholder='...' className='input-field-checkout'/>
-                        </form>
+                    <input
+                        type="tel"
+                        id="phone-number"
+                        name="phoneNumber"
+                        placeholder='Phone Number'
+                        className='input-field-shorter'
+                        value={phoneNumber}
+                        onChange={(e) => setPhoneNumber(e.target.value)}
+                    />
+
+                    {/* Bank Details Section */}
+                    <h3 className="text-checkout" style={{paddingTop :20}}>Payment Method </h3>
+                    <p className='text-checkout'>Pay With Card</p>
+                    <div style = {{alignContent: 'center'}}>
+                        <input 
+                            type="text" 
+                            id="card-name" 
+                            name="cardName" 
+                            placeholder='Name On Card' 
+                            className='input-field-shorter' 
+                            value={cardName}
+                            onChange={(e) => setCardName(e.target.value)}
+                        />
                     </div>
-                </div>
-
-                <p className="text-checkout">Phone Number </p>
-                <form className='checkout-form'>
-                    <input type="text" id="phone-number" name="phone-number" placeholder='...' className='input-field-shorter'/>
-                </form>
-                
-                <h3 className="text-checkout" style={{paddingTop :20}}>Payment Method </h3>
-                <p className='text-checkout'>Pay With Card</p>
-
-                <div style = {{alignContent: 'center'}}>
-                    <p className="text-checkout">Name On Card </p>
-                    <form className='checkout-form'>
-                        <input type="text" id="card-name" name="card-name" placeholder='Card holder' className='input-field-shorter' />
-                    </form>
-                </div>
-                    <p className="text-checkout">Card Number </p>
-                    <form className='checkout-form'>
-                        <input type="number" id="card-number" name="card-number" placeholder='...' className='input-field-shorter'/>
-                    </form>
-                    
+                    <input 
+                        type="number" 
+                        id="card-number" 
+                        name="cardNumber" 
+                        placeholder='Card Number' 
+                        className='input-field-shorter'
+                        value={cardNumber}
+                        onChange={(e) => setCardNumber(e.target.value)}
+                    />
                     <p className="text-checkout">Expiry date </p>
-                    <form className='checkout-form'>
-                        <input type="number" id="address" name="address" placeholder='...' className='input-field-shorter'/>
-                    </form>
-                    <p className="text-checkout">Security Code </p>
-                    <form className='checkout-form'>
-                        <input type="number" id="" name="address" placeholder='...' className='input-field-shorter'/>
-                    </form>
-                
+                    <input 
+                        type="number" 
+                        id="exp-month" 
+                        name="expiryMonth" 
+                        placeholder='MM'
+                        maxLength={2} 
+                        className='input-field-shorter'
+                        value={expiryMonth}
+                        onChange={(e) => setExpiryMonth(e.target.value)}
+                    />
+                    <p>/</p>
+                    <input 
+                        type="number" 
+                        id="exp-year" 
+                        name="expiryYear" 
+                        placeholder='YY'
+                        maxLength={2} 
+                        className='input-field-shorter'
+                        value={expiryYear}
+                        onChange={(e) => setExpiryYear(e.target.value)}
+                    />
+                    <input 
+                        type="number" 
+                        id="security-code" 
+                        name="securityCode" 
+                        placeholder='000'
+                        maxLength={3} 
+                        className='input-field-shorter'
+                        value={securityCode}
+                        onChange={(e) => setSecurityCode(e.target.value)}
+                    />
 
+                    {/* Checkout button */}
+                    { !isLoading && <button className='checkout-button'>Checkout and View Store Map</button> }
+                    { isLoading && <button className='checkout-button' disabled>Processing...</button> }
+                </form>
             </div>
 
 
@@ -103,10 +178,11 @@ const StoreCheckoutPage = () => {
         <div className='invoice-1'>
             <InvoiceCopy products={products} quantities={quantities} />
         </div>
-        <button className='checkout-button' onClick={handleClick}>
+        
+        {/* <button className='checkout-button' onClick={handleClick}>
             Checkout and View Store Map
         </button>
-
+        */}
         </div>
     )
 }
